c50681e28d8764984b41853dd9eb1de436a310c2
kk <>
2021-01-27 12:37:18 +0900

    original validation 追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
     before_action :set_article, only: [:show, :edit, :update]
 
     def index
-        @articles = Article
+        @articles = Article.all
     end
 
     def show
