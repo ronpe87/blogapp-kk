ea1a1a354e607e81ebfbeceeebe41c1465333681
kk <>
2021-02-01 21:44:01 +0900

    day21詰まり修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,27 +7,34 @@
 #  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_articles_on_user_id  (user_id)
 #
 class Article < ApplicationRecord
-    validates :title, presence:true
-    validates :title, length:{ minimum: 2, maximum: 100 }
-    validates :title, format: { with: /\A(?!\@)/ }
+  validates :title, presence:true
+  validates :title, length:{ minimum: 2, maximum: 100 }
+  validates :title, format: { with: /\A(?!\@)/ }
 
-    validates :content, presence: true
-    validates :content, length:{ minimum: 10 }
-    validates :content, uniqueness: true
+  validates :content, presence: true
+  validates :content, length:{ minimum: 10 }
+  validates :content, uniqueness: true
 
-    validate :validate_title_and_content_length
+  validate :validate_title_and_content_length
 
-    def display_created_at
-        I18n.l(self.created_at, format: :default)
+  belongs_to :user
+
+  def display_created_at
+    I18n.l(self.created_at, format: :default)
+  end
+
+  private
+  def validate_title_and_content_length
+    char_count = self.title.length + self.content.length
+    unless char_count > 100
+        errors.add(:content, '100文字以上で頼む')
     end
-
-    private
-    def validate_title_and_content_length
-        char_count = self.title.length + self.content.length
-        unless char_count > 100
-            errors.add(:content, '100文字以上で頼む')
-        end
-    end
+  end
 end
