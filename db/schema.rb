479200135c71d140993cb1cf8f502106fad31912
kk <>
2021-02-02 14:15:02 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_28_123102) do
+ActiveRecord::Schema.define(version: 2021_01_31_030204) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
