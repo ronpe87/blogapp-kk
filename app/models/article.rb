ebaff973fdc41f569ee47cdb11352810c5592dae
kk <>
2021-03-22 16:32:23 +0900

    Revert "ActionTextを使う"
    This reverts commit 71f64e15b8bf5a3fda4e1711c1dd6aef7e505b45.

--- app/models/article.rb
+++ app/models/article.rb
@@ -21,6 +21,10 @@ class Article < ApplicationRecord
   validates :title, format: { with: /\A(?!\@)/ }
 
   validates :content, presence: true
+  validates :content, length:{ minimum: 10 }
+  validates :content, uniqueness: true
+
+  validate :validate_title_and_content_length
 
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
@@ -36,5 +40,11 @@ class Article < ApplicationRecord
 
   def like_count
     likes.count
+  end
+
+  private
+  def validate_title_and_content_length
+      char_count = self.title.length + self.content.length
+          errors.add(:content, '100文字以上で頼む') unless char_count > 100
   end
 end
