d3b6218ce4914064049545c68d38e35b28324251
kk <>
2021-03-23 14:43:21 +0900

    フォロー機能1

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,10 +21,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :articles, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :favorite_article, through: :likes, source: :article
+
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   has_one :profile, dependent: :destroy
 
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
@@ -39,6 +46,15 @@ class User < ApplicationRecord
 
   def display_name
     profile&.nickname || self.email.split('@').first
+  end
+
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
+  end
+
+  def unfollow!(user)
+    relation = following_relationships.find_by!(followoing_id: user.id)
+    relation.destroy!
   end
 
   def prepare_profile
