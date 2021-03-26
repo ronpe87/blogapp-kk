3916c98184d0c7b94480a343cff30ba443818544
kk <>
2021-03-26 22:47:36 +0900

    Ajaxによるコメント表示

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -12,11 +12,50 @@ const handleHeartDisplay = (hasLiked) => {
   }
 }
 
+const handleCommentForm = () => {
+  $('.show-comment-form').on('click', () => {
+    $('.show-comment-form').addClass('hidden')
+    $('.comment-text-area').removeClass('hidden')
+  })
+}
+
+const appendNewComment = (comment) => {
+  $('.comments-container').append(
+    `<div class="article_comment"><p>${comment.content}</p></div>`
+  )
+}
+
 document.addEventListener('turbolinks:load', () => {
   const dataset = $('#article-show').data()
   const articleId = dataset.articleId
 
-  axios.get(`/articles/1/like`)
+  axios.get(`/articles/${articleId}/comments`)
+    .then((response) => {
+      const comments = response.data
+      comments.forEach((comment) => {
+        appendNewComment(comment)
+      })
+    })
+
+    handleCommentForm()
+
+    $('.add-comment-button').on('click', () => {
+      const content = $('#comment_content').val()
+      if (!content) {
+        window.alert('コメントを入力してください')
+      } else {
+        axios.post(`/articles/${articleId}/comments`, {
+          comment: {content: content}
+        })
+          .then((res) => {
+            const comment = res.data
+            appendNewComment(comment)
+            $('#comment_content').val('')
+          })
+      }
+    })
+
+    axios.get(`/articles/1/like`)
     .then((response) => {
       const hasLiked = response.data.hasLiked
       handleHeartDisplay(hasLiked)
